167595d1eb4298a95af811c305848ec3c7858409
Chun-gu <chungu.yi92@gmail.com>
2024-03-15 01:08:19 +0900

    test

---
+++ tsup.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig((options) => {
+  return {
+    entry: ["src/index.ts"],
+    format: ["esm"],
+    minify: !options.watch,
+    splitting: false,
+    sourcemap: false,
+    clean: true,
+  };
+});
